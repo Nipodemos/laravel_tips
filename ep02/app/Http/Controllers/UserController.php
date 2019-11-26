faaf69d1a65319d55ec1e138c9f6c74b75837c80
nipodemos <nipodemos13@gmail.com>
2019-11-25 21:06:29 -0300

    finalização do ep02

--- ep02/app/Http/Controllers/UserController.php
+++ ep02/app/Http/Controllers/UserController.php
@@ -2,12 +2,23 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\User;
+use Illuminate\Contracts\View\View;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
     public function listUser()
     {
-        echo "<h1> Listagem de Usuario </h1>";
+        // $user = new User();
+        // $user->name = "Alan";
+        // $user->email = 'nipodemos13@gmail.com';
+        // $user->password = Hash::make('123');
+        // $user->save();
+        $user = User::where('id', '=', 1)->first();
+        // dd($user);
+        return View('listUser', [
+            'user' => $user
+        ]);
     }
 }
